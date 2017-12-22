39c027bc528c6810db092d5c5fe28ceb5461376f
Juan Camilo Isaza <jkmisaza@gmail.com>
2017-12-22 11:54:10 -0500

    App Component Unit Test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,85 +1,47 @@
-import {TestBed, async} from '@angular/core/testing';
 import {AppComponent} from './app.component';
-import {LoginComponent} from './states/login/login.component';
-import {RegisterComponent} from './states/register/register.component';
-import {DashboardComponent} from './states/dashboard/dashboard.component';
-import {NavComponent} from './commons/component/nav/nav.component';
-import {DetailComponent} from './states/detail/detail.component';
-import {HeaderComponent} from './commons/component/header/header.component';
-import {STATES} from './states/states';
-import {TranslateLoader, TranslateModule} from '@ngx-translate/core';
-import {HTTP_INTERCEPTORS, HttpClient, HttpClientModule} from '@angular/common/http';
-import {MomentModule} from 'angular2-moment';
-import {BrowserModule} from '@angular/platform-browser';
-import {CoreModule} from './core/core.module';
-import {uiRouterConfigFn} from './config/router.config';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-import {UIRouterModule} from '@uirouter/angular';
-import {translateLoader} from './config/multi-translate-http-loader';
-import {FormsModule} from '@angular/forms';
+import {
+  MissingTranslationHandler, TranslateCompiler, TranslateLoader, TranslateModule, TranslateParser, TranslateService
+} from '@ngx-translate/core';
+import {DebugElement, Injector} from '@angular/core';
+import {ComponentFixture, inject, TestBed} from '@angular/core/testing';
+import {UIRouterModule} from "@uirouter/angular";
 import {APP_BASE_HREF} from "@angular/common";
-import {GeneralInterceptor} from "./app.interceptor";
+
+class TranslateServiceMock extends TranslateService {
+
+}
 
 fdescribe('AppComponent', () => {
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [
-        AppComponent,
-        DashboardComponent,
-        NavComponent,
-        DetailComponent,
-        HeaderComponent,
-        RegisterComponent,
-        LoginComponent
-      ],
-      imports: [
-        BrowserModule,
-        UIRouterModule.forRoot({
-          states: STATES,
-          useHash: false,
-          config: uiRouterConfigFn
-        }),
-        CoreModule,
-        MomentModule,
-        FormsModule,
-        HttpClientModule,
-        TranslateModule.forRoot({
-          loader: {
-            provide: TranslateLoader,
-            useFactory: translateLoader,
-            deps: [HttpClient]
-          }
-        }),
-        NgbModule.forRoot()
-      ],
-      providers: [
-        {
-          provide: HTTP_INTERCEPTORS,
-          useClass: GeneralInterceptor,
-          multi: true
-        },
-        {
-          provide: APP_BASE_HREF, useValue: '/'
-        }
-      ]
-    }).compileComponents();
-  }));
+  let comp: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let de: DebugElement;
+  let el: HTMLElement;
 
-  it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
-  }));
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [AppComponent], // declare the test component
+      imports: [
+        UIRouterModule.forRoot(),
+        TranslateModule.forRoot()
+      ],
+      providers: [{provide: APP_BASE_HREF, useValue: '/'}]
+    });
+
+    fixture = TestBed.createComponent(AppComponent);
+
+    comp = fixture.componentInstance; // BannerComponent test instance
+
+    // query for the title <h1> by CSS element selector
+    // de = fixture.debugElement.query(By.css('h1'));
+    // el = de.nativeElement;
+  });
+
+  afterEach(() => {
+    // translate = undefined;
+  });
+
+  it('should create', () => {
+    expect(comp).toBeTruthy();
+  });
 });
