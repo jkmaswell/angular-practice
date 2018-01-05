0fc75819ae984f5ad5810d383ecb72529b6f3539
Juan Camilo Isaza <jkmisaza@gmail.com>
2018-01-05 13:16:29 -0500

    adding enum object to router config

--- src/app/config/router.config.ts
+++ src/app/config/router.config.ts
@@ -1,22 +1,27 @@
-import {StateService, UIRouter} from '@uirouter/angular';
+import {UIRouter} from '@uirouter/angular';
 import {Injector} from '@angular/core';
 import {UserService} from '../core/service/user/user.service';
+
+enum Auth {
+  Yes = 'logged',
+  No = 'notLogged'
+}
 
 /** UIRouter Config  */
 export function uiRouterConfigFn(router: UIRouter, injector: Injector) {
   const criteria = {entering: (state) => state.data && state.data.authorization};
   router.transitionService.onBefore(criteria, requireAuthentication);
-
   router.urlService.rules.otherwise({state: 'login'});
 }
 
 function requireAuthentication(transition) {
   const $state = transition.router.stateService;
   const authSvc = transition.injector().get(UserService);
+  const stateData = transition.targetState().$state().data;
   return authSvc.getUser().toPromise().then(currentUser => {
-    if (transition.targetState().$state().data.authorization === 'logged' && !currentUser) {
+    if (stateData.authorization === Auth.Yes && !currentUser) {
       return $state.target('login');
-    } else if (transition.targetState().$state().data.authorization === 'notLogged' && currentUser) {
+    } else if (stateData.authorization === Auth.No && currentUser) {
       return $state.target('main.dashboard');
     }
   });
